bd2b6cc634cf662703d03f0e53d44db6636c539f
Christian Lück <christian@lueck.tv>
2014-12-16 16:51:22 +0100

    Forcefully terminate Process if its Stream closes

--- tests/ProcessLauncherTest.php
+++ tests/ProcessLauncherTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use Clue\React\Shell\ProcessLauncher;
+use React\Stream\ReadableStream;
 
 class ProcessLauncherTest extends TestCase
 {
@@ -24,5 +25,33 @@ class ProcessLauncherTest extends TestCase
         $shell = $this->processLauncher->createDeferredShell($process);
 
         $this->assertInstanceOf('Clue\React\Shell\DeferredShell', $shell);
+    }
+
+    public function testClosingStreamTerminatesRunningProcess()
+    {
+        $process = $this->getMockBuilder('React\ChildProcess\Process')->disableOriginalConstructor()->getMock();
+        $process->stdout = new ReadableStream();
+        $process->stdin = $this->getMock('React\Stream\WritableStreamInterface');
+
+        $process->expects($this->once())->method('isRunning')->will($this->returnValue(true));
+        $process->expects($this->once())->method('terminate')->with($this->equalTo(SIGKILL));
+
+        $shell = $this->processLauncher->createDeferredShell($process);
+
+        $shell->close();
+    }
+
+    public function testClosingStreamOfNonRunningProcessWillNotTerminate()
+    {
+        $process = $this->getMockBuilder('React\ChildProcess\Process')->disableOriginalConstructor()->getMock();
+        $process->stdout = new ReadableStream();
+        $process->stdin = $this->getMock('React\Stream\WritableStreamInterface');
+
+        $process->expects($this->once())->method('isRunning')->will($this->returnValue(false));
+        $process->expects($this->never())->method('terminate');
+
+        $shell = $this->processLauncher->createDeferredShell($process);
+
+        $shell->close();
     }
 }
